4ca5b938302d2ebcdfb3eee21a83072f7a34322b
JoRyGu <gude.joshua@gmail.com>
2019-09-26 23:07:30 -0400

    Finished Backend CRUD for Activities

---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,37 @@
+﻿using Domain;
+using MediatR;
+using Persistence;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                _context.Activities.Remove(new Activity { Id = request.Id });
+                var wasDeletedSuccessfully = await _context.SaveChangesAsync() > 0;
+
+                if (wasDeletedSuccessfully) return Unit.Value;
+
+                throw new Exception("There was a problem when attempting to delete this record.");
+            }
+        }
+    }
+}
